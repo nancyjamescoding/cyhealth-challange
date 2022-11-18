02a04ad70796569e91b10177c00a946210eb4c5d
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-11-18 18:14:06 +0300

    Created a script.js page to help in  fetching the statistical api

---
+++ script.js
@@ -0,0 +1,48 @@
+const fetchCovidStats = async (country) => {
+    const options = {
+        headers: {
+            'X-RapidAPI-Key': "571ae676c1mshaeed6da803ffe85p186b49jsn509a986b4c81",
+            'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+          }
+    }
+    const requestUrl = country == null ? 
+    `https://covid-193.p.rapidapi.com/statistics` : 
+    `https://covid-193.p.rapidapi.com/statistics?country=${country}`;
+
+    return await fetch(requestUrl, options)
+    .then(response => response.json())
+	.then(response => response.response)
+	.catch(() => {return []});
+}
+
+const displayStats = async (country=null) => {
+    const results = await fetchCovidStats(country)
+    
+    if (results.length == 0) {
+        alert(`Could not get the results of country - ${country}`)
+    }
+    
+    let covidResultsTable = document.getElementById('covid-results-table').getElementsByTagName('tbody')[0]
+    let tableContent = ""
+    results.forEach((item, index) => {
+        tableContent += `
+        <tr>
+            <td>${item.country}</td>
+            <td>${item.deaths.total}</td>
+            <td>${item.tests.total}</td>
+            <td>${item.cases.total}</td>
+            <td>${item.population}</td>
+            <td>${item.day}</td>
+        </tr>`
+    });
+    covidResultsTable.innerHTML = tableContent
+}
+
+const searchByCountry = async (event) => {
+    event.preventDefault()
+    if (event.keyCode === 13) {
+        country = document.getElementById('country-search').value
+        await displayStats(country)
+    }
+    
+}
